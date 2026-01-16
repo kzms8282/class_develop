b55ad646b5bdcfaf67de84f12742b2bb6283a6b3
takuya-maker <ngn2549547@stu.o-hara.ac.jp>
2026-01-16 15:42:22 +0900

    名前の追加とコードの追加

--- src/ImageOutput.py
+++ src/ImageOutput.py
@@ -1,4 +1,12 @@
 from PIL import Image
 
-def image_output():
-    pass
+def image_output(image, save):
+    height = len(image)
+    width = len(image[0])
+    size = height * width
+    img = Image.new("RGB", (width, height))
+
+    array = [] # 2次元配列を1次元に開く
+    for i in image:
+        array += i
+    img.putdata()
